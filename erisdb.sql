5ff057b4bc05b0485ec03220bf5c36378fd7e6b5
Rishi Goswami <rishigoswami91@gmail.com>
2025-12-01 21:38:08 +0530

    Updated registration form validation

--- erisdb.sql
+++ erisdb.sql
@@ -1,26 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 04, 2018 at 06:22 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2025 at 04:59 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `erisdb`
 --
-CREATE DATABASE IF NOT EXISTS `erisdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `erisdb`;
 
 -- --------------------------------------------------------
 
@@ -28,8 +27,8 @@ USE `erisdb`;
 -- Table structure for table `tblapplicants`
 --
 
-CREATE TABLE IF NOT EXISTS `tblapplicants` (
-  `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblapplicants` (
+  `APPLICANTID` int(11) NOT NULL,
   `FNAME` varchar(90) NOT NULL,
   `LNAME` varchar(90) NOT NULL,
   `MNAME` varchar(90) NOT NULL,
@@ -45,9 +44,8 @@ CREATE TABLE IF NOT EXISTS `tblapplicants` (
   `CONTACTNO` varchar(90) NOT NULL,
   `DEGREE` text NOT NULL,
   `APPLICANTPHOTO` varchar(255) NOT NULL,
-  `NATIONALID` varchar(255) NOT NULL,
-  PRIMARY KEY (`APPLICANTID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2018016 ;
+  `NATIONALID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblapplicants`
@@ -56,7 +54,10 @@ CREATE TABLE IF NOT EXISTS `tblapplicants` (
 INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `SEX`, `CIVILSTATUS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `USERNAME`, `PASS`, `EMAILADDRESS`, `CONTACTNO`, `DEGREE`, `APPLICANTPHOTO`, `NATIONALID`) VALUES
 (2018013, 'Kim', 'Domingo', 'Enoe', 'Kab City', 'Female', 'none', '1991-01-01', 'Kab Citys', 27, 'kim', 'a6312121e15caec74845b7ba5af23330d52d4ac0', 'kim@y.com', '5415456', 'BSAC', 'photos/RobloxScreenShot20180406_203758793.png', ''),
 (2018014, 'Jake', 'Zyrus', 'Ilmba', 'Kab City', 'Female', 'none', '1993-01-16', 'Kab City', 25, 'jake', 'c8d99c2f7cd5f432c163abcd422672b9f77550bb', 'jake@y.com', '14655623123123', 'BSIT', '', ''),
-(2018015, 'Janry', 'Tan', 'Lim', 'brgy 1 Kab City', 'Female', 'Single', '1992-01-30', 'Kab City', 26, 'janry', '1dd4efc811372cd1efe855981a8863d10ddde1ca', 'jan@gmail.com', '0234234', 'BSIT', '', '');
+(2018015, 'Janry', 'Tan', 'Lim', 'brgy 1 Kab City', 'Female', 'Single', '1992-01-30', 'Kab City', 26, 'janry', '1dd4efc811372cd1efe855981a8863d10ddde1ca', 'jan@gmail.com', '0234234', 'BSIT', '', ''),
+(2025016, 'RISHIBHARTHI', 'GOSWAMI', 'Sureshbharthi', 'SUNCITY-1,NEAR VADLA FATAK,SHAPUR,JUNAGADH', 'Female', 'Single', '2004-10-15', 'JUNAGADH', 21, 'rishi', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'rishigoswami91@gmail.com', '7863872065', 'Running MCA', 'photos/Myimg.jpg', ''),
+(2025017, 'Goswami', 'Mahedhgiri', 'Harsh', 'Jnd', 'Male', 'none', '2002-01-01', 'JUNAGADH', 23, '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', 'h@gmail.com', '7863872065', '', '', ''),
+(2025018, 'Satish', 'Kindarkhediya', 'Parbat bhay', 'Rajkot', 'Male', 'Single', '2004-01-01', 'Rajkor', 21, 'Satish', '7c4a8d09ca3762af61e59520943dc26494f8941b', 's@gmail.com', '7894561230', 'BCA', '', '');
 
 -- --------------------------------------------------------
 
@@ -64,14 +65,13 @@ INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`
 -- Table structure for table `tblattachmentfile`
 --
 
-CREATE TABLE IF NOT EXISTS `tblattachmentfile` (
-  `FILEID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblattachmentfile` (
+  `FILEID` int(11) NOT NULL,
   `JOBID` int(11) NOT NULL,
   `FILE_NAME` varchar(90) NOT NULL,
   `FILE_LOCATION` varchar(255) NOT NULL,
-  `USERATTACHMENTID` int(11) NOT NULL,
-  PRIMARY KEY (`FILEID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2147483647 ;
+  `USERATTACHMENTID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblattachmentfile`
@@ -86,14 +86,13 @@ INSERT INTO `tblattachmentfile` (`FILEID`, `JOBID`, `FILE_NAME`, `FILE_LOCATION`
 -- Table structure for table `tblautonumbers`
 --
 
-CREATE TABLE IF NOT EXISTS `tblautonumbers` (
-  `AUTOID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblautonumbers` (
+  `AUTOID` int(11) NOT NULL,
   `AUTOSTART` varchar(30) NOT NULL,
   `AUTOEND` int(11) NOT NULL,
   `AUTOINC` int(11) NOT NULL,
-  `AUTOKEY` varchar(30) NOT NULL,
-  PRIMARY KEY (`AUTOID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `AUTOKEY` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblautonumbers`
@@ -101,9 +100,9 @@ CREATE TABLE IF NOT EXISTS `tblautonumbers` (
 
 INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
 (1, '02983', 7, 1, 'userid'),
-(2, '000', 78, 1, 'employeeid'),
-(3, '0', 16, 1, 'APPLICANT'),
-(4, '69125', 29, 1, 'FILEID');
+(2, '000', 79, 1, 'employeeid'),
+(3, '0', 19, 1, 'APPLICANT'),
+(4, '69125', 30, 1, 'FILEID');
 
 -- --------------------------------------------------------
 
@@ -111,11 +110,10 @@ INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTO
 -- Table structure for table `tblcategory`
 --
 
-CREATE TABLE IF NOT EXISTS `tblcategory` (
-  `CATEGORYID` int(11) NOT NULL AUTO_INCREMENT,
-  `CATEGORY` varchar(90) NOT NULL,
-  PRIMARY KEY (`CATEGORYID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
+CREATE TABLE `tblcategory` (
+  `CATEGORYID` int(11) NOT NULL,
+  `CATEGORY` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblcategory`
@@ -141,15 +139,14 @@ INSERT INTO `tblcategory` (`CATEGORYID`, `CATEGORY`) VALUES
 -- Table structure for table `tblcompany`
 --
 
-CREATE TABLE IF NOT EXISTS `tblcompany` (
-  `COMPANYID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblcompany` (
+  `COMPANYID` int(11) NOT NULL,
   `COMPANYNAME` varchar(90) NOT NULL,
   `COMPANYADDRESS` varchar(90) NOT NULL,
   `COMPANYCONTACTNO` varchar(30) NOT NULL,
   `COMPANYSTATUS` varchar(90) NOT NULL,
-  `COMPANYMISSION` text NOT NULL,
-  PRIMARY KEY (`COMPANYID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+  `COMPANYMISSION` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblcompany`
@@ -157,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `tblcompany` (
 
 INSERT INTO `tblcompany` (`COMPANYID`, `COMPANYNAME`, `COMPANYADDRESS`, `COMPANYCONTACTNO`, `COMPANYSTATUS`, `COMPANYMISSION`) VALUES
 (2, 'URC', 'Bry Camugao', '023654', '', 'weqwe'),
-(3, 'Copreros', 'Mabinay''s', '035656', '', ''),
+(3, 'Copreros', 'Mabinay\'s', '035656', '', ''),
 (4, 'Quest', 'Kabankalan City', '23165', '', ''),
 (6, 'Palacios Company', 'Kabankalan City', '0625656899', '', ''),
 (7, 'IT Company', 'Kabankalan City', '04564123', '', '');
@@ -168,8 +165,8 @@ INSERT INTO `tblcompany` (`COMPANYID`, `COMPANYNAME`, `COMPANYADDRESS`, `COMPANY
 -- Table structure for table `tblemployees`
 --
 
-CREATE TABLE IF NOT EXISTS `tblemployees` (
-  `INCID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblemployees` (
+  `INCID` int(11) NOT NULL,
   `EMPLOYEEID` varchar(30) NOT NULL,
   `FNAME` varchar(50) NOT NULL,
   `LNAME` varchar(50) NOT NULL,
@@ -189,17 +186,15 @@ CREATE TABLE IF NOT EXISTS `tblemployees` (
   `EMPUSERNAME` varchar(90) NOT NULL,
   `EMPPASSWORD` varchar(125) NOT NULL,
   `DATEHIRED` date NOT NULL,
-  `COMPANYID` int(11) NOT NULL,
-  PRIMARY KEY (`INCID`),
-  UNIQUE KEY `EMPLOYEEID` (`EMPLOYEEID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=77 ;
+  `COMPANYID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblemployees`
 --
 
 INSERT INTO `tblemployees` (`INCID`, `EMPLOYEEID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `SEX`, `CIVILSTATUS`, `TELNO`, `EMP_EMAILADDRESS`, `CELLNO`, `POSITION`, `WORKSTATS`, `EMPPHOTO`, `EMPUSERNAME`, `EMPPASSWORD`, `DATEHIRED`, `COMPANYID`) VALUES
-(76, '2018001', 'Chambe', 'Narciso', 'Captain', 'mabinay', '1992-01-23', 'Mabinay', 26, 'Male', 'Married', '032656', 'chambe@yahoo.com', '', 'Fuel Tender', '', '', '2018001', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', '2018-05-23', 2);
+(77, '1', 'Ramesh', 'Chavda', 'Hagdish', 'Rajkot', '2000-01-01', 'Rajkot', 25, 'Male', 'Single', '7894561230', 'r@fgmail.com', '', 'Employee', '', '', '1', '356a192b7913b04c54574d18c28d46e6395428ab', '2025-12-01', 3);
 
 -- --------------------------------------------------------
 
@@ -207,13 +202,12 @@ INSERT INTO `tblemployees` (`INCID`, `EMPLOYEEID`, `FNAME`, `LNAME`, `MNAME`, `A
 -- Table structure for table `tblfeedback`
 --
 
-CREATE TABLE IF NOT EXISTS `tblfeedback` (
-  `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblfeedback` (
+  `FEEDBACKID` int(11) NOT NULL,
   `APPLICANTID` int(11) NOT NULL,
   `REGISTRATIONID` int(11) NOT NULL,
-  `FEEDBACK` text NOT NULL,
-  PRIMARY KEY (`FEEDBACKID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `FEEDBACK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -221,8 +215,8 @@ CREATE TABLE IF NOT EXISTS `tblfeedback` (
 -- Table structure for table `tbljob`
 --
 
-CREATE TABLE IF NOT EXISTS `tbljob` (
-  `JOBID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbljob` (
+  `JOBID` int(11) NOT NULL,
   `COMPANYID` int(11) NOT NULL,
   `CATEGORY` varchar(90) NOT NULL,
   `OCCUPATIONTITLE` varchar(90) NOT NULL,
@@ -234,17 +228,16 @@ CREATE TABLE IF NOT EXISTS `tbljob` (
   `PREFEREDSEX` varchar(30) NOT NULL,
   `SECTOR_VACANCY` text NOT NULL,
   `JOBSTATUS` varchar(90) NOT NULL,
-  `DATEPOSTED` datetime NOT NULL,
-  PRIMARY KEY (`JOBID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `DATEPOSTED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tbljob`
 --
 
 INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
-(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00'),
-(2, 2, 'Technology', 'Accounting', 1, 15000, 'may 20', 'Two years Experience', 'We are looking for bachelor of science in Acountancy', 'Female', 'yes', '', '2018-05-20 02:33:00');
+(1, 7, 'HR', 'ISD', 2, 39000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00'),
+(2, 3, 'Shipping', 'Accounting', 1, 25000, 'may 20', 'Three years Experience', 'We are looking for bachelor of Commerace in Acountancy', 'Male', 'yes', '', '2018-05-20 02:33:00');
 
 -- --------------------------------------------------------
 
@@ -252,8 +245,8 @@ INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_
 -- Table structure for table `tbljobregistration`
 --
 
-CREATE TABLE IF NOT EXISTS `tbljobregistration` (
-  `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbljobregistration` (
+  `REGISTRATIONID` int(11) NOT NULL,
   `COMPANYID` int(11) NOT NULL,
   `JOBID` int(11) NOT NULL,
   `APPLICANTID` int(11) NOT NULL,
@@ -261,19 +254,17 @@ CREATE TABLE IF NOT EXISTS `tbljobregistration` (
   `REGISTRATIONDATE` date NOT NULL,
   `REMARKS` varchar(255) NOT NULL DEFAULT 'Pending',
   `FILEID` int(11) NOT NULL,
-  `PENDINGAPPLICATION` tinyint(1) NOT NULL DEFAULT '1',
-  `HVIEW` tinyint(1) NOT NULL DEFAULT '1',
-  `DATETIMEAPPROVED` datetime NOT NULL,
-  PRIMARY KEY (`REGISTRATIONID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `PENDINGAPPLICATION` tinyint(1) NOT NULL DEFAULT 1,
+  `HVIEW` tinyint(1) NOT NULL DEFAULT 1,
+  `DATETIMEAPPROVED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tbljobregistration`
 --
 
 INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLICANTID`, `APPLICANT`, `REGISTRATIONDATE`, `REMARKS`, `FILEID`, `PENDINGAPPLICATION`, `HVIEW`, `DATETIMEAPPROVED`) VALUES
-(1, 2, 2, 2018013, 'Kim Domingo', '2018-05-27', 'Ive seen your work and its really interesting', 2147483647, 0, 1, '2018-05-26 16:13:01'),
-(2, 2, 2, 2018015, 'Janry Tan', '2018-05-26', 'aasd', 2147483647, 0, 0, '2018-05-28 14:14:45');
+(3, 3, 2, 2025016, 'RISHIBHARTHI GOSWAMI', '2025-11-30', 'Pending', 2147483647, 1, 1, '2025-11-30 16:35:00');
 
 -- --------------------------------------------------------
 
@@ -281,23 +272,147 @@ INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLI
 -- Table structure for table `tblusers`
 --
 
-CREATE TABLE IF NOT EXISTS `tblusers` (
+CREATE TABLE `tblusers` (
   `USERID` varchar(30) NOT NULL,
   `FULLNAME` varchar(40) NOT NULL,
   `USERNAME` varchar(90) NOT NULL,
   `PASS` varchar(90) NOT NULL,
   `ROLE` varchar(30) NOT NULL,
-  `PICLOCATION` varchar(255) NOT NULL,
-  PRIMARY KEY (`USERID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `PICLOCATION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `tblusers`
 --
 
 INSERT INTO `tblusers` (`USERID`, `FULLNAME`, `USERNAME`, `PASS`, `ROLE`, `PICLOCATION`) VALUES
-('00018', 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', 'photos/Koala.jpg'),
+('00018', 'admin', 'admin', 'f865b53623b121fd34ee5426c792e5c33af8c227', 'Administrator', 'photos/user.jpg'),
+('1', 'Ramesh Chavda', 'Chavda', '356a192b7913b04c54574d18c28d46e6395428ab', 'Employee', ''),
 ('2018001', 'Employee', 'Employee', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', 'Employee', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  ADD PRIMARY KEY (`APPLICANTID`);
+
+--
+-- Indexes for table `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  ADD PRIMARY KEY (`FILEID`);
+
+--
+-- Indexes for table `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  ADD PRIMARY KEY (`AUTOID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`CATEGORYID`);
+
+--
+-- Indexes for table `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  ADD PRIMARY KEY (`COMPANYID`);
+
+--
+-- Indexes for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`INCID`),
+  ADD UNIQUE KEY `EMPLOYEEID` (`EMPLOYEEID`);
+
+--
+-- Indexes for table `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  ADD PRIMARY KEY (`FEEDBACKID`);
+
+--
+-- Indexes for table `tbljob`
+--
+ALTER TABLE `tbljob`
+  ADD PRIMARY KEY (`JOBID`);
+
+--
+-- Indexes for table `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  ADD PRIMARY KEY (`REGISTRATIONID`);
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2025019;
+
+--
+-- AUTO_INCREMENT for table `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  MODIFY `FILEID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
+
+--
+-- AUTO_INCREMENT for table `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  MODIFY `AUTOID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `CATEGORYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  MODIFY `COMPANYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `INCID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+
+--
+-- AUTO_INCREMENT for table `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbljob`
+--
+ALTER TABLE `tbljob`
+  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
